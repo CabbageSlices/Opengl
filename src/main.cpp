c952627160981da02c893bf2544e33663b413b24
Nimesh <nimesh.shrestha@hotmail.com>
2018-09-23 22:41:03 -0700

    Added sfml and glad to ssetup opengl functionaliy

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
+#include "glad/glad.h"
+#include "SFML/OpenGL.hpp"
+#include "SFML/Window.hpp"
 
 using std::cout;
 using std::endl;
 
 int main(int argc, char const *argv[])
 {
-	cout << "Hello World!" << endl;
+	
+	//setup opengl context with version 4.5 core
+	sf::Window window(sf::VideoMode(800, 600), "OpenGL",
+		sf::Style::Default, sf::ContextSettings(24, 8, 4, 4, 5, sf::ContextSettings::Core));
+
+	//activte the context
+	window.setActive(true);
+
+	//load the extension functions since sfml doesn't do it for you
+	if(!gladLoadGL()) {
+		cout << "Unable to load opengl" << endl;
+		exit(-1);
+	}
+
+	cout << GLVersion.major << " " << GLVersion.minor << endl;
+
+	bool isRunning = true;
+
+	while(isRunning) {
+
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if(event.type == sf::Event::Closed)
+				isRunning = false;
+		}
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		window.display();
+	}
 	return 0;
 }
