1999f5ce912e68acf904ddad10ea252dab75a015
cabbageslices <nimesh.shrestha@hotmail.com>
2019-10-24 20:10:23 -0700

    remove unneeded variables in meshdata

--- src/headers/MeshData.h
+++ src/headers/MeshData.h
@@ -2,7 +2,9 @@
 
 #include <vector>
 #include <string>
+#include <memory>
 #include <map>
+#include "Material.h"
 #include "glm\glm.hpp"
 
 struct MeshAttributes {
@@ -20,17 +22,28 @@ struct MeshAttributes {
 
 //3 indices to make up a face
 //each index should refer to position, normal, and texcoords
-typedef glm::ivec3 Face;
+struct Face {
+	unsigned i1, i2, i3;
+};
+
+// typedef glm::uvec3 Face;
 
 //collection of faces, this way all indices are consequtive and laid out in memory linearly so you can send them to opengl
 typedef std::vector<Face> FaceSet;
 
+//mesh data works as follows:
+/**
+ * Entire mesh is split into groups of faces that share a single material, this group of faces is called a faceset.
+ * each faceset is referenced by it's material id, so to get a group of faces you use the material ID.
+ * A face has 3 verticies, so once you have a face set, you can index each individual face in the set.
+ * A face is composed of 3 indices, 1 for each vertex. The index will simultaneously index into the postiion, normal, and texcoord array.
+ * So a face with indices 0, 1, 2 means it uses position/normal/texcoord [0], [1], and [2]
+ * All mesh attribtue data is stored in the MeshAttributes object.
+ * If the texcoord or normal array is empty then the mesh does not contain any texcoord or normal data, only position
+ * It's up to the user to make sure they don't utilize the mepty materials.*/
 struct MeshData {
 
 	void clear() {
-		positions.clear();
-		normals.clear();
-		indices.clear();
 
 		attributes.clear();
 
@@ -38,14 +51,13 @@ struct MeshData {
 	}
 
 	MeshAttributes attributes;
-	
-	std::vector<glm::vec3> positions;
-	std::vector<glm::vec3> normals;
-
 
 	//map of material name to the faces that use that material
 	//maps material id to faceset
 	std::map<int, FaceSet> materialFaceMap;
 
-	std::vector<unsigned int> indices;
+	//total number of indices so you don't have to iterate through the facemap
+	int numIndices;
+
+	std::vector<std::shared_ptr<Material> > materials;
 };
